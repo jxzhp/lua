9b84686262d5d64c5498bc9005ea23cf1015fca5
kabir-zhong <15618537095@126.com>
2016-02-02 17:00:33 +0800

    format

--- fact.lua
+++ fact.lua
@@ -1,11 +1,11 @@
 #!/usr/bin/env lua
 
 function fact (n)
-	if n == 0 then
-		return 1
-	else
-		return n * fact(n-1)
-	end
+  if n == 0 then
+    return 1
+  else
+    return n * fact(n-1)
+  end
 end
 
 print("Enter a number:")
--- for.lua
+++ for.lua
@@ -1,11 +1,11 @@
 #!/usr/bin/env lua
 a = {}
-for i=1,20 do 
-	a[i] = i
+for i=1,20 do
+  a[i] = i
 end
 
 for i=1, #a do
-	print(a[i])
+  print(a[i])
 end
 
 print(table.maxn(a))
